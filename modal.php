6fcb456607b6d93889578d34caa394b8f52d3e1e
bhargav-BL <bhargavarampentyala@gmail.com>
2025-12-15 10:47:48 +0530

    asdadsac d

--- modal.php
+++ modal.php
@@ -5,27 +5,25 @@
     <p>Please fill in the details below to continue browsing.</p>
 
     <form id="contactForm">
+
       <input type="text" name="name" placeholder="Full Name" required>
-    <input type="tel" name="phone" placeholder="Phone Number" required><select name="Subject" class="no-nice-select" required>
-          <option value="">Select Enquiry*</option>
-          <option value="site_visit">Schedule a Site Visit</option>
-          <option value="price_details">Price & Cost Details</option>
-          <option value="floor_plans">Floor Plans & Layouts</option>
-          <option value="availability">Availability of Villas</option>
-          <option value="brochure_download">Download Brochure</option>
-          <option value="other">Other</option>
-        </select>
+
+      <input type="tel" name="phone" placeholder="Phone Number" required>
+
+      <select name="Subject" class="no-nice-select" required>
+        <option value="">Select Enquiry*</option>
+        <option value="site_visit">Schedule a Site Visit</option>
+        <option value="price_details">Price & Cost Details</option>
+        <option value="floor_plans">Floor Plans & Layouts</option>
+        <option value="availability">Availability of Villas</option>
+        <option value="brochure_download">Download Brochure</option>
+        <option value="other">Other</option>
+      </select>
+
       <textarea name="message" placeholder="Your Message" required></textarea>
 
       <button type="submit">Submit</button>
+
     </form>
   </div>
 </div>
-
-
-<script>
-  $(document).ready(function () {
-  $('select').not('.no-nice-select').niceSelect();
-});
-
-</script>
