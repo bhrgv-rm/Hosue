9f0391997fb65a376eb5e5c770eefbb7a0988c5c
bhargav-BL <bhargavarampentyala@gmail.com>
2025-12-15 10:56:43 +0530

    asdsadafc

--- modal.php
+++ modal.php
@@ -8,26 +8,46 @@
     <h2>Contact Us</h2>
     <p>Please fill in the details below to continue browsing.</p>
 
-    <form id="contactForm">
+<form id="footerContactForm">
+  <div class="row">
 
-      <input type="text" name="name" placeholder="Full Name" required>
+    <div class="col-lg-6">
+      <div class="input-area">
+        <input type="text" name="Name" placeholder="Your Name*" required>
+      </div>
+    </div>
 
-      <input type="tel" name="phone" placeholder="Phone Number" required>
+    <div class="col-lg-6">
+      <div class="input-area">
+        <input type="number" name="Mobile" placeholder="Mobile Number*" required>
+      </div>
+    </div>
 
-      <select name="Subject" class="no-nice-select" required>
-        <option value="">Select Enquiry*</option>
-        <option value="site_visit">Schedule a Site Visit</option>
-        <option value="price_details">Price & Cost Details</option>
-        <option value="floor_plans">Floor Plans & Layouts</option>
-        <option value="availability">Availability of Villas</option>
-        <option value="brochure_download">Download Brochure</option>
-        <option value="other">Other</option>
-      </select>
+    <div class="col-lg-6">
+      <div class="input-area">
+        <select name="Subject" required>
+          <option value="">Select Enquiry*</option>
+          <option value="site_visit">Schedule a Site Visit</option>
+          <option value="price_details">Price & Cost Details</option>
+          <option value="floor_plans">Floor Plans & Layouts</option>
+          <option value="availability">Availability of Villas</option>
+          <option value="brochure_download">Download Brochure</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+    </div>
 
-      <textarea name="message" placeholder="Your Message" required></textarea>
+    <div class="col-lg-6">
+      <div class="input-area">
+        <textarea name="Message" placeholder="Your Message*" required></textarea>
+      </div>
+    </div>
 
-      <button type="submit">Submit</button>
+    <div class="col-lg-6 text-center">
+      <button type="submit" class="header-btn4">Send Message</button>
+    </div>
 
-    </form>
+  </div>
+</form>
   </div>
 </div>
