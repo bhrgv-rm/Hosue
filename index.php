2d33a639da2330b1dafa4174aac2e31cef367167
bhargav-BL <bhargavarampentyala@gmail.com>
2025-12-15 22:06:25 +0530

    sdacsdc

--- index.php
+++ index.php
@@ -391,209 +391,31 @@
       <div class="row">
         <div class="col-lg-5">
           <div class="testimonia-header heading3">
-            <h5 data-aos="fade-left" data-aos-duration="800">client feedback</h5>
-            <div class="space20"></div>
-            <h2 class="text-anime-style-3">Hear What Our Client Say About Property</h2>
+            <h5 data-aos="fade-left" data-aos-duration="800">master plans</h5>
             <div class="space16"></div>
             <p data-aos="fade-left" data-aos-duration="1000">Indulge in the epitome of luxury living at Luxury, where every <br class="d-lg-block d-none"> amenity is meticulously designed to elevate your lifestyle.</p>
             <div class="space32"></div>
-            <div class="btn-area1" data-aos="fade-left" data-aos-duration="1200">
-              <a href="testimonial.php" class="header-btn4">View All Testimonials</a>
-            </div>
-          </div>
-          <div class="space86"></div>
-          <div class="testimonial-video-area" data-aos="zoom-in-up" data-aos-duration="1000">
-            <div class="img1">
-              <img src="/assets/img/all-images/testimonial/testimonial-img3.png" alt="">
-            </div>
-            <div class="icons">
-              <a href="https://www.youtube.com/watch?v=Y8XpQpW5OVY" class="popup-youtube"><i class="fa-solid fa-play"></i></a>
-            </div>
           </div>
         </div>
-        <div class="col-lg-1"></div>
-        <div class="col-lg-6">
-          <div class="testimonial-arrow-area" data-aos="zoom-in-up" data-aos-duration="1000">
-            <div class="testimonial3-vertical-slider">
-              <div class="testimonial-vertical">
-                <div class="verical-boxarea">
-                  <div class="images-area">
-                    <div class="img1">
-                      <img src="/assets/img/all-images/testimonial/testimonial-img4.png" alt="">
-                    </div>
-                    <div class="text">
-                      <a href="#">Alexy Queen</a>
-                      <p>Happy Client</p>
-                    </div>
-                  </div>
-                  <div class="quito">
-                    <img src="/assets/img/icons/quoto-icon2.svg" alt="">
-                  </div>
-                </div>
-                <div class="space24"></div>
-                <span>Highly recommend Suite Luxury Suite Villa!</span>
-                <div class="space16"></div>
-                <p>“After staying at Luxury, I can confidently say it was one of the most luxurious best experiences of luxury villa.” </p>
-                <div class="space24"></div>
-                <ul>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                </ul>
-              </div>
-              <div class="testimonial-vertical">
-                <div class="verical-boxarea">
-                  <div class="images-area">
-                    <div class="img1">
-                      <img src="/assets/img/all-images/testimonial/testimonial-img5.png" alt="">
-                    </div>
-                    <div class="text">
-                      <a href="#">Jofra Archer</a>
-                      <p>Happy Client</p>
-                    </div>
-                  </div>
-                  <div class="quito">
-                    <img src="/assets/img/icons/quoto-icon2.svg" alt="">
-                  </div>
-                </div>
-                <div class="space24"></div>
-                <span>Unforgettable Experience Of Luxury & Comfort!</span>
-                <div class="space16"></div>
-                <p>"Celebrated my anniversary at Property Villa, and it was beyond magical. The attention to detail, the of serene.”</p>
-                <div class="space24"></div>
-                <ul>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                </ul>
-              </div>
-              <div class="testimonial-vertical">
-                <div class="verical-boxarea">
-                  <div class="images-area">
-                    <div class="img1">
-                      <img src="/assets/img/all-images/testimonial/testimonial-img4.png" alt="">
-                    </div>
-                    <div class="text">
-                      <a href="#">Alexy Queen</a>
-                      <p>Happy Client</p>
-                    </div>
-                  </div>
-                  <div class="quito">
-                    <img src="/assets/img/icons/quoto-icon2.svg" alt="">
-                  </div>
-                </div>
-                <div class="space24"></div>
-                <span>Highly recommend Suite Luxury Suite Villa!</span>
-                <div class="space16"></div>
-                <p>“After staying at Luxury, I can confidently say it was one of the most luxurious best experiences of luxury villa.” </p>
-                <div class="space24"></div>
-                <ul>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                </ul>
-              </div>
-              <div class="testimonial-vertical">
-                <div class="verical-boxarea">
-                  <div class="images-area">
-                    <div class="img1">
-                      <img src="/assets/img/all-images/testimonial/testimonial-img5.png" alt="">
-                    </div>
-                    <div class="text">
-                      <a href="#">Jofra Archer</a>
-                      <p>Happy Client</p>
-                    </div>
-                  </div>
-                  <div class="quito">
-                    <img src="/assets/img/icons/quoto-icon2.svg" alt="">
-                  </div>
-                </div>
-                <div class="space24"></div>
-                <span>Unforgettable Experience Of Luxury & Comfort!</span>
-                <div class="space16"></div>
-                <p>"Celebrated my anniversary at Property Villa, and it was beyond magical. The attention to detail, the of serene.”</p>
-                <div class="space24"></div>
-                <ul>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                </ul>
-              </div>
-              <div class="testimonial-vertical">
-                <div class="verical-boxarea">
-                  <div class="images-area">
-                    <div class="img1">
-                      <img src="/assets/img/all-images/testimonial/testimonial-img4.png" alt="">
-                    </div>
-                    <div class="text">
-                      <a href="#">Alexy Queen</a>
-                      <p>Happy Client</p>
-                    </div>
-                  </div>
-                  <div class="quito">
-                    <img src="/assets/img/icons/quoto-icon2.svg" alt="">
-                  </div>
-                </div>
-                <div class="space24"></div>
-                <span>Highly recommend Suite Luxury Suite Villa!</span>
-                <div class="space16"></div>
-                <p>“After staying at Luxury, I can confidently say it was one of the most luxurious best experiences of luxury villa.” </p>
-                <div class="space24"></div>
-                <ul>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                </ul>
-              </div>
-              <div class="testimonial-vertical">
-                <div class="verical-boxarea">
-                  <div class="images-area">
-                    <div class="img1">
-                      <img src="/assets/img/all-images/testimonial/testimonial-img5.png" alt="">
-                    </div>
-                    <div class="text">
-                      <a href="#">Jofra Archer</a>
-                      <p>Happy Client</p>
-                    </div>
-                  </div>
-                  <div class="quito">
-                    <img src="/assets/img/icons/quoto-icon2.svg" alt="">
-                  </div>
-                </div>
-                <div class="space24"></div>
-                <span>Unforgettable Experience Of Luxury & Comfort!</span>
-                <div class="space16"></div>
-                <p>"Celebrated my anniversary at Property Villa, and it was beyond magical. The attention to detail, the of serene.”</p>
-                <div class="space24"></div>
-                <ul>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                  <li><i class="fa-solid fa-star"></i></li>
-                </ul>
-              </div>
-            </div>
-            <div class="testimonial-arrows">
-              <div class="testimonial-prev-arrow">
-                <button><i class="fa-solid fa-angle-up"></i></button>
-              </div>
-              <div class="testimonial-next-arrow">
-                <button><i class="fa-solid fa-angle-down"></i></button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div class="swiper">
+  <!-- Additional required wrapper -->
+  <div class="swiper-wrapper">
+    <!-- Slides -->
+    <div class="swiper-slide">Slide 1</div>
+    <div class="swiper-slide">Slide 2</div>
+    <div class="swiper-slide">Slide 3</div>
+    ...
+  </div>
+  <!-- If we need pagination -->
+  <div class="swiper-pagination"></div>
+
+  <!-- If we need navigation buttons -->
+  <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
+
+  <!-- If we need scrollbar -->
+  <div class="swiper-scrollbar"></div>
+</div>
       </div>
     </div>
   </div>
@@ -654,7 +476,7 @@
 
     <div class="col-lg-12 mt-4">
       <div class="input-area">
-        <input name="Email" style="padding: 8px; margin-bottom: 12px;border: 1px solid gray; border-radius: 4px; width: 100%;" required>
+        <input name="Email" placeholder="Email*" style="padding: 8px; margin-bottom: 12px;border: 1px solid gray; border-radius: 4px; width: 100%;" required>
       </div>
     </div>
 
