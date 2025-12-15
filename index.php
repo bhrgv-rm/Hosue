a1f40a922633e03e85ce769c902ac3962eb039aa
bhargav-BL <bhargavarampentyala@gmail.com>
2025-12-15 11:08:13 +0530

    sdc vcxd

--- index.php
+++ index.php
@@ -625,12 +625,42 @@
     <p>Please fill in the details below to continue browsing.</p>
 
     <form id="contactForm">
-      <input type="text" name="Name" placeholder="Full Name" required>
-      <input type="tel" name="Phone" placeholder="Phone Number" required>
-      <input name="Subject" value="-" style="display:hidden">
-      <textarea name="Message" placeholder="Your Message" required></textarea>
+          <div class="col-lg-6">
+      <div class="input-area">
+        <input type="text" name="Name" placeholder="Your Name*" required>
+      </div>
+    </div>
 
-      <button type="submit">Submit</button>
+    <div class="col-lg-6">
+      <div class="input-area">
+        <input type="number" name="Mobile" placeholder="Mobile Number*" required>
+      </div>
+    </div>
+
+    <div class="col-lg-12 mt-4">
+      <div class="input-area">
+        <select name="Subject" required>
+          <option value="">Select Enquiry*</option>
+          <option value="site_visit">Schedule a Site Visit</option>
+          <option value="price_details">Price & Cost Details</option>
+          <option value="floor_plans">Floor Plans & Layouts</option>
+          <option value="availability">Availability of Villas</option>
+          <option value="brochure_download">Download Brochure</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+    </div>
+
+    <div class="col-lg-12 mt-4">
+      <div class="input-area">
+        <textarea name="Message" placeholder="Your Message*" required></textarea>
+      </div>
+    </div>
+
+    <div class="col-lg-12 text-end">
+      <button type="submit" class="header-btn4">Send Message</button>
+    </div>
+
     </form>
   </div>
 </div>
